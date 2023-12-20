934365f4ad0d1181d4c2b59410b7a3adaa569dd6
Ashish8901 <ashish8901@gmail.com>
2023-12-20 16:05:06 +0530

    bug fixed for vercel

--- middleware.js
+++ middleware.js
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/",
-};
+// export const config = {
+//   matcher: "/",
+// };
